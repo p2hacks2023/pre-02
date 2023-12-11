bea3a60dde04c546aba7320ee5a41820d1a8ae9a
Takuto <tiharu717@gmail.com>
2023-12-11 10:27:55 +0900

    [fix] getAllPosts 投稿時間でソートされるようにした

--- lib/view_model/multi/posts_repository.dart
+++ lib/view_model/multi/posts_repository.dart
@@ -1,16 +1,17 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_tutorial/model/post.dart';
 import 'package:flutter/foundation.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 class PostsRepository {
   CollectionReference _postsRef = FirebaseFirestore.instance.collection('posts');
   
   Future<void> getAllPosts() async{
     List<Post> posts; 
-    _postsRef.get().then(
+    _postsRef.orderBy('post_datetime', descending: true).get().then(
       (QuerySnapshot) {
         for(var docSnapshot in QuerySnapshot.docs) {
-          debugPrint(docSnapshot.data().toString());
+          debugPrint(docSnapshot.data().toString() + docSnapshot.id);
         }
       }
     );
@@ -23,7 +24,8 @@ class PostsRepository {
         'description': _post.description,
         'image_url': _post.image_url.toString(),
         'favorite_array': [],
-        'post_datetime': _post.post_datetime,
+        //'post_datetime': _post.post_datetime,
+        'post_datetime': FieldValue.serverTimestamp(),
       });
     }catch(Exception){
       debugPrint(Exception.toString());
