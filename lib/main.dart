80304abcad3f9f88b9ea25015792e26dba209f31
Takuto <tiharu717@gmail.com>
2023-12-12 10:12:20 +0900

    [fix] macosのinfo.plist

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_tutorial/firebase_options.dart';
 import 'package:firebase_tutorial/routes.dart';
 import 'package:flutter/material.dart';
