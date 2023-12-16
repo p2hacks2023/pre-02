ab456a45f7cb303d0bb078891e1c742f79105037
Takuto <tiharu717@gmail.com>
2023-12-16 22:08:30 +0900

    [fix]

--- lib/view/Yoru/yoru.dart
+++ lib/view/Yoru/yoru.dart
@@ -118,7 +118,6 @@ class Yoru extends ConsumerWidget {
           'https://cdn.discordapp.com/attachments/1181202431116312719/1185187126946574416/42_20231215204918.PNG?ex=658eb286&is=657c3d86&hm=e6c6a9b4b5890e96204f40f6539e55e6fa32618455e78d01a130b8a8244b7e57&'
           ),
       ),
-      backgroundColor: Color(0xFF190831),
       //backgroundColor: Colors.transparent,
     );
   }
